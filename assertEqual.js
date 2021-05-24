aedcf5af9daa8df42382fb02d882d6e96f28b005
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-05-24 20:40:33 +0000

    Chnages

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
-const assertEquals = function(actual, expected) {
+const assertEqual = function(actual, expected) {
     if (actual === expected)
-        console.log(":-) Assertion Passed: " + actual + " === " + expected);
+        console.log(`Assertion Passed: ${actual} === ${expected}`);
     else
-        console.log("Assertion Failed: " + actual + " !== " + expected);
+        console.log(`Assertion Failed: ${actual} !== ${expected}`);
 };
 
 
 // TEST CODE
-assertEquals("Lighthouse Labs", "Lighthouse Labs");
-assertEquals(1, 0.1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 0.1);
